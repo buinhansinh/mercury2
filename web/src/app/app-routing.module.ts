3200b6ae0c19fcbb60228377c49e8bd02a9d4db5
terence <terence_so@pegatroncorp.com>
2018-03-09 18:04:30 +0800

    Added routing and toolbar

--- web/src/app/app-routing.module.ts
+++ web/src/app/app-routing.module.ts
@@ -1,12 +1,16 @@
-import {
-    NgModule
-} from '@angular/core';
-import {
-    RouterModule,
-    Routes
-} from '@angular/router';
+import { NgModule } from "@angular/core";
+import { RouterModule, Routes } from "@angular/router";
+import { DashboardComponent } from "./dashboard/dashboard.component";
+import { AdminComponent } from "./admin/admin.component";
+
+const routes: Routes = [
+  { path: "", redirectTo: "/dashboard", pathMatch: "full" },
+  { path: "dashboard", component: DashboardComponent },
+  { path: "admin", component: AdminComponent }
+];
 
 @NgModule({
-    exports: [RouterModule]
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
 })
 export class AppRoutingModule {}
--- web/src/app/app.module.ts
+++ web/src/app/app.module.ts
@@ -1,38 +1,38 @@
 import {
-    BrowserAnimationsModule,
-    NoopAnimationsModule,
-} from '@angular/platform-browser/animations';
+  BrowserAnimationsModule,
+  NoopAnimationsModule
+} from "@angular/platform-browser/animations";
 import {
+  MatButtonModule,
+  MatCheckboxModule,
+  MatSidenavModule,
+  MatToolbarModule,
+  MatIconModule,
+  MatInputModule,
+  MatAutocompleteModule,
+} from "@angular/material";
+import { BrowserModule } from "@angular/platform-browser";
+import { NgModule } from "@angular/core";
+import { AppComponent } from "./app.component";
+import { AppRoutingModule } from "./app-routing.module";
+import { DashboardComponent } from "./dashboard/dashboard.component";
+import { AdminComponent } from "./admin/admin.component";
+
+@NgModule({
+  declarations: [AppComponent, DashboardComponent, AdminComponent],
+  imports: [
+    AppRoutingModule,
+    BrowserModule,
+    BrowserAnimationsModule,
     MatButtonModule,
     MatCheckboxModule,
     MatSidenavModule,
     MatToolbarModule,
     MatIconModule,
-} from '@angular/material';
-import {
-    BrowserModule
-} from '@angular/platform-browser';
-import {
-    NgModule
-} from '@angular/core';
-import {
-    AppComponent
-} from './app.component';
-
-@NgModule({
-    declarations: [
-        AppComponent
-    ],
-    imports: [
-        BrowserModule,
-        BrowserAnimationsModule,
-        MatButtonModule,
-        MatCheckboxModule,
-        MatSidenavModule,
-        MatToolbarModule,
-        MatIconModule,
-    ],
-    providers: [],
-    bootstrap: [AppComponent]
+    MatInputModule,
+    MatAutocompleteModule,
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
 })
 export class AppModule {}
